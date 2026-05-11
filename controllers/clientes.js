8d2185c08f386e66bb82d8a44f19cfd0fb87f262
AndreaMartinezC12 <andre.martcer@gmail.com>
2026-05-10 19:49:28 -0600

    Conectando con la database que estare usando en lugar de usar valores dados desde el codigo

--- controllers/clientes.js
+++ controllers/clientes.js
@@ -1,126 +1,122 @@
-let clientes = [
-    { id: 1, nombre: 'Cris', email:'cris12@gmail.com', telefono:'6145637829' },
-    { id: 2, nombre: 'Juan', email:'juanillo@gmail.com', telefono: '6146379287'}
-  ];
-  
-  const getClientes = (req, res) => {
-    res.json(clientes);
-  };
-  
-  const getClienteById = (req, res) => {
-    const id = parseInt(req.params.id);
-    const cliente = clientes.find(c => c.id === id);
-  
-    if (!cliente) {
-      return res.status(404).json({
-        error: 'Cliente no encontrado',
-        message: `No existe un cliente con el id ${id}`
-      });
-    }
-  
-    res.json(cliente);
-  };
-  
-  const createCliente = (req, res) => {
-    const {nombre, email, telefono} = req.body
+const db=require('../config/db')
 
+const getClientes=async(req,res)=>{
+  try {
+    const [rows]= await db.query('SELECT * FROM cliente')
+    res.json(rows)
+  } catch (error) {
+    console.error('Error al obtener clientes', error)
+    res.status(500).json({error:'Error al obtener clientes'})
+  }
+}
+
+const getClienteById=async(req,res)=>{
+  try {
+    const id= parseInt(req.params.id)
+    const [rows] = await db.query('SELECT * FROM cliente WHERE idCliente =?', [id])
+
+    if(rows.length===0){
+      return res.status(404).json({error: "Cliente no encontrado"})
+    }
+    res.json(rows[0])
+
+  } catch (error) {
+    console.error('Error al obtener cliente', error)
+    res.status(500).json({error:'Error al obtener cliente'})
+  }
+}
+
+const createCliente=async(req,res)=>{
+  try {
+    const {nombre, email, telefono} = req.body
     if(!nombre || nombre.trim()===''){
-        return res.status(400).json({
-            error:"El nombre es obligatorio"
-        })
-    }
-
-    if (!email || email.trim() === '') {
-        return res.status(400).json({
-            error: "El email es obligatorio"
-        })
-    }
-
-    if (!telefono || telefono.trim() === '') {
-        return res.status(400).json({
-            error: "El telefono es obligatorio"
-        })
-    }
-
-    const nuevo = {
-        id:clientes.length > 0 ? clientes[clientes.length - 1].id + 1 : 1, 
-        nombre,
-        email,
-        telefono
-    }
-
-    clientes.push(nuevo);
-  
-    res.status(201).json(nuevo);
-  };
-  
-  const updateCliente = (req, res) => {
-    const id = parseInt(req.params.id);
-    const index = clientes.findIndex(c => c.id === id);
-  
-    if (index === -1) {
-      return res.status(404).json({
-        error: 'Cliente no encontrado',
-        message: `No existe un cliente con el id ${id}`
-      });
-    }
-  
-    const {nombre, email, telefono} = req.body
-  
-    if (!nombre || nombre.trim() === '') {
       return res.status(400).json({
-        error: 'El nombre es obligatorio'
-      });
+        error:'El nombre es obligatorio'
+      })
     }
 
-    if (!email || email.trim() === '') {
-        return res.status(400).json({
-            error: "El email es obligatorio"
-        })
+    if(!email || email.trim()===''){
+      return res.status(400).json({
+        error:'El email es obligatorio'
+      })
     }
 
-    if (!telefono || telefono.trim() === '') {
-        return res.status(400).json({
-            error: "El telefono es obligatorio"
-        })
+    if(!telefono || telefono.trim()===''){
+      return res.status(400).json({
+        error:'El telefono es obligatorio'
+      })
     }
-  
-    clientes[index] = {
-      ...clientes[index],
-      nombre: nombre.trim(),
-      email,
-      telefono
-    };
-  
-    res.json({
-      message: 'Cliente actualizado',
-      cliente: clientes[index]
-    });
-  };
-  
-  const deleteCliente = (req, res) => {
-    const id = parseInt(req.params.id);
-    const index = clientes.findIndex(c => c.id === id);
-  
-    if (index === -1) {
+    const[result]= await db.query('INSERT INTO cliente (nombre, email, telefono) VALUES (?, ?, ?)', [nombre, email, telefono])
+    const [newCliente]= await db.query('SELECT * FROM cliente WHERE idCliente = ?', [result.insertId])
+
+    res.status(201).json(newCliente[0])
+
+  } catch (error) {
+    console.error('Error al crear cliente', error)
+    res.status(500).json({error:'Error al crear cliente'})
+  }
+}
+
+const updateCliente = async(req,res)=>{
+  try {
+    const id= parseInt(req.params.id)
+    const {nombre} = req.body
+    if(!nombre||nombre.trim()===''){
+      return res.status(400).json({
+        error:'El nombre es obligatorio'
+      })
+    }
+    
+    const [existing]= await db.query('SELECT * FROM cliente WHERE idCliente = ?', [id])
+
+    if(existing.length===0){
       return res.status(404).json({
-        error: 'Cliente no encontrado',
-        message: `No existe un cliente con el id ${id}`
-      });
+        error:'Cliente no encontrado'
+      })
     }
-  
-    const clienteEliminado = clientes.splice(index, 1);
-  
+
+    await db.query('UPDATE cliente SET nombre = ? WHERE idCliente = ?', [nombre, id])
+    const[updateCliente]=await db.query('SELECT * FROM cliente WHERE idCliente = ?', [id])
+
     res.json({
-      message: 'Cliente eliminado',
-      cliente: clienteEliminado[0]
-    });
-  };
-  
-  module.exports = {
-    getClientes,
-    getClienteById,
-    createCliente,
-    updateCliente,
-    deleteCliente
-  };
+      message:'Usuario actualizado',
+      cliente:updateCliente[0]
+    })
+
+  } catch (error) {
+    console.error('Error al actualizar cliente', error)
+    res.status(500).json({error:'Error al actualizar cliente'})
+  }
+}
+
+const deleteCliente = async(req,res)=>{
+  try {
+    const id= parseInt(req.params.id)
+    const [existing]= await db.query('SELECT * FROM cliente WHERE idCliente = ?', [id])
+
+    if(existing.length===0){
+      return res.status(404).json({
+        error:'Cliente no encontrado'
+      })
+    }
+
+    await db.query('DELETE FROM cliente WHERE idCliente = ?', [id])
+
+    res.json({
+      message:"Cliente eliminado",
+      cliente:existing[0]
+    })
+
+  } catch (error) {
+    console.error('Error al eliminar cliente', error)
+    res.status(500).json({error:'Error al eliminar cliente'})
+  }
+}
+
+module.exports={
+  getClientes,
+  getClienteById,
+  createCliente,
+  updateCliente,
+  deleteCliente
+}
--- index.js
+++ index.js
@@ -4,13 +4,27 @@ const destinatarioRoutes= require('./routes/destinatarios')
 const pastelRoutes= require('./routes/pasteles')
 const pagoRoutes= require('./routes/pagos')
 
+const db=require('./config/db')
+
 const app=express()
+app.use(express.json())
 
-app.use('/clientes', clienteRoutes)
-app.use('/destinatarios', destinatarioRoutes)
-app.use('/pasteles', pastelRoutes)
-app.use('/pagos', pagoRoutes)
+app.use('/cliente', clienteRoutes)
+app.use('/destinatario', destinatarioRoutes)
+app.use('/pastel', pastelRoutes)
+app.use('/pago', pagoRoutes)
 
-app.listen(3015, ()=>{
-    console.log('servidor corriendo')
-})
+const testDBConnectionAndStart = async() =>{
+    try {
+        await db.query('SELECT 1')
+        console.log('Ta funcionando')
+        app.listen(3015, ()=>{
+            console.log('servidor corriendo')
+        })
+    } catch (error) {
+        console.error('Tas mal con la db', error.message)
+        process.exit(1)
+    }
+}
+
+testDBConnectionAndStart()
