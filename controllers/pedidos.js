7184421681408440e4ea34faeed7eda69877f9fa
AndreaMartinezC12 <andre.martcer@gmail.com>
2026-05-16 23:18:02 -0600

    Agregando mas informacion al resultado de la busqueda por nombre

--- controllers/pedidos.js
+++ controllers/pedidos.js
@@ -257,7 +257,7 @@ const getPedidoById = async (req, res) => {
 
       pastel: {
         id: row.pastelId,
-        tamado: row.tamano,
+        tamano: row.tamano,
         pan: row.pan,
         relleno: row.relleno,
         extras: row.extras,
@@ -288,8 +288,6 @@ const getPedidoByNombre = async (req, res) => {
   try {
     const nombre = req.query.nombre
 
-    console.log("SI ENTRE")
-
     const [rows] = 
       await db.query(
         `
@@ -297,15 +295,29 @@ const getPedidoByNombre = async (req, res) => {
           pedido.id AS pedidoId,
           pedido.estado,
 
+          cliente.id AS clienteId,
           cliente.nombre AS clienteNombre,
           cliente.email,
           cliente.telefono,
 
+          destinatario.id AS destinatarioId,
           destinatario.nombre AS destinatarioNombre,
+          destinatario.calle,
+          destinatario.numero,
+          destinatario.colonia,
+          destinatario.fechaEntrega,
 
+          pastel.id AS pastelId,
           pastel.tamano,
+          pastel.pan,
+          pastel.relleno,
+          pastel.extras,
+          pastel.betun,
+          pastel.decoracion,
 
-          pago.total
+          pago.id AS pagoId,
+          pago.total,
+          pago.metodo
 
         FROM pedido
 
@@ -325,7 +337,50 @@ const getPedidoByNombre = async (req, res) => {
         `,
         [`%${nombre}%`]
       )
-      res.json(rows)
+      if(rows.length === 0){
+      return res.status(404).json({
+        error: "Pedido no encontrado"
+      })
+    }
+
+    const row = rows[0]
+
+    const pedido ={
+      pedidoId:
+        row.pedidoId,
+      
+      estado:
+        row.estado,
+
+      cliente: {
+        nombre: row.clienteNombre,
+        email: row.email,
+        telefono:row.telefono
+      },
+
+      destinatario: {
+        nombre: row.destinatarioNombre,
+        calle: row.calle,
+        numero: row.numero,
+        colonia: row.colonia,
+        fechaEntrega: row.fechaEntrega
+      },
+
+      pastel: {
+        tamano: row.tamano,
+        pan: row.pan,
+        relleno: row.relleno,
+        extras: row.extras,
+        betun: row.betun,
+        decoracion: row.decoracion
+      },
+
+      pago: {
+        total: row.total,
+        metodo: row.metodo
+      }
+    }
+    res.json(pedido)
 
   } catch (error) {
     console.log(error)
